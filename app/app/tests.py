97b19adfb50d1b80afad7f67609ab8838a95fce3
Aman <aman.asingh307@gmail.com>
2024-01-09 00:23:59 +0530

    fixed assertion value

--- app/app/tests.py
+++ app/app/tests.py
@@ -8,7 +8,7 @@ class CalcTests(SimpleTestCase):
     def test_add_numbers(self):
         res = calc.add(5, 5)
 
-        self.assertEqual(res, 11)
+        self.assertEqual(res, 10)
 
 
 # class CalcTestsSub(SimpleTestCase):
